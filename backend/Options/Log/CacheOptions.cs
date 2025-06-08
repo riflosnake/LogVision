ff03dd28284d1ddeefc087cf88715014adb674a7
Flori Batusha <Flori.Batusha@raiffeisen.al>
2025-06-08 16:30:47 +0200

    Add cache options

---
+++ backend/Options/Log/CacheOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace LogInfoApi.Options.Log;
+
+public class CacheOptions
+{
+    public required EndpointCacheOption GetLogs { get; set; }
+    public required EndpointCacheOption GetLogTimeseries { get; set; }
+    public required EndpointCacheOption GetLogTypes { get; set; }
+}
---
+++ backend/Options/Log/EndpointCacheOption.cs
@@ -0,0 +1,8 @@
+﻿namespace LogInfoApi.Options.Log;
+
+public class EndpointCacheOption
+{
+    public bool IsEnabled { get; set; }
+    public int Stale { get; set; }
+    public int Total { get; set; }
+}
